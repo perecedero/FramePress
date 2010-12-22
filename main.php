6ef553bac26a70ed8c4c44a1dce553b6547b0aa8
Perecedero <perecedero@gmail.com>
2010-12-22 12:17:41 -0300

    added some examples of pages and actions on main.php

--- main.php
+++ main.php
@@ -19,10 +19,30 @@ Author URI: http://infinimedia.com/
 	/*
 		'tools' =>array(//[menu, dashboard, posts, media, links, pages, comments, appearance, plugins, users, tools, settings]
 			array(
-				'page_title' => 'AppliedSeo',
-				'menu_title' => 'SEO',
+				'page_title' => 'Tools Title 1',
+				'menu_title' => 'Tools Mtitle 1',
 				'capability' => 'administrator',
-				'menu_slug' => 'principal', //page name
+				'menu_slug' => 'principal_controller',
+			),
+			array(
+				'page_title' => 'Tools Title 2',
+				'menu_title' => 'Tools Mtitle 2',
+				'capability' => 'administrator',
+				'menu_slug' => 'second_controller',
+			),
+		),
+		'dashboard' =>array(//[menu, dashboard, posts, media, links, pages, comments, appearance, plugins, users, tools, settings]
+			array(
+				'page_title' => 'dashboard Title 1',
+				'menu_title' => 'dashboard Mtitle 1',
+				'capability' => 'administrator',
+				'menu_slug' => 'principal_controller',
+			),
+			array(
+				'page_title' => 'dashboard Title 2',
+				'menu_title' => 'dashboard Mtitle 2',
+				'capability' => 'administrator',
+				'menu_slug' => 'second_controller',
 			),
 		),
 	*/
@@ -31,13 +51,19 @@ Author URI: http://infinimedia.com/
 	$wp_actions = array (
 	/*
 		array(
-			tag' => 'appliedseo_init_process',
-			handler' => 'principal',
-			function' => 'appliedseo_init_process',
+			tag' => 'some_tag',
+			handler' => 'principal_controller',
+			function' => 'afunction_under_controller',
 			'is_ajax' => true,
 		),
+		array(
+			tag' => 'some_tag_2',
+			handler' => 'therd_ctrler',
+			function' => 'index',
+			'is_ajax' => false,
+		),
 	*/
-    );
+	);
 
 	$w2pf_test->Page->add($wp_pages);
 	$w2pf_test->Action->add($wp_actions);
