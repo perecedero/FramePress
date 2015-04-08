9f5c1434ca72e3062102d3c58f2c554156df8d48
perecedero <perecedero@gmail.com>
2015-04-08 10:13:12 -0300

    Changed wordpress module interface
    way more semantic to interact with wordpress

--- Core/Lib/WordPress.php
+++ Core/Lib/WordPress.php
@@ -1,8 +1,8 @@
 <?php
 
 //define core class
-if (!class_exists('FramePress_WordPress_001')) {
-class FramePress_WordPress_001
+if (!class_exists('FramePress_WordPress_002')) {
+class FramePress_WordPress_002
 {
 
 	public $pages = array();
@@ -18,107 +18,73 @@ class FramePress_WordPress_001
 	public function __construct(&$fp)
 	{
 		$this->Core = $fp;
-	}
-
-	/**
-	 * Register admin pages
-	 *
-	 * @param array $pages
-	 * @return void
-	 * @link http://codex.wordpress.org/Administration_Menus
-	*/
-	public function adminPages( $pages=array() )
-	{
-		$this->pages = $pages;
-
-		//Populate page with default/missing/computed info
-		foreach ($this->pages as $type => $pages){
-			for($i=0; $i<count($pages); $i++){
-
-				//populate missing/default info
-				$page_defaults = array('page.title'=> null, 'menu.title'=> null, 'capability'=> null, 'controller'=> null, 'function'=>'index', 'parent'=> null, 'icon'=> null, 'position'=> null);
-				$page = array_merge($page_defaults, $pages[$i]);
-
-				//generate url for image selected
-				if ( $page['icon'] && file_exists( $this->Core->paths['img'] . DS . $page['icon'])) {
-					$page['icon'] =  $this->Core->paths['img.url'] . DS . $page['icon'];
-				}
-
-				//generate slug for this menu
-				$page['menu.slug'] = sanitize_title($this->Core->config['prefix'] . ' ' . $page['menu.title']);
-
-				//find parent menu
-				if ($page['parent']){
-					$menus = (isset($this->pages['menu']))?$this->pages['menu']:array();
-					for ($p=0; $p < count($menus); $p++){
-						if( $menus[$p]['menu.title'] == $page['parent'] ) {
-							$page['parent.slug'] = sanitize_title($this->Core->config['prefix'] . ' ' . $menus[$p]['page.title']);
-							break;
-						}
-					}
-					if(!isset($page['parent.slug'] )){ $page['parent.slug'] = $page['parent']; }
-				}
-				$this->pages[$type][$i] = $page;
-			}
-		}
 
 		add_action('admin_menu', array($this, 'addAdminPagesReal'));
+		add_action('add_meta_boxes', array($this, 'addMetaboxesReal'));
 	}
 
-	/**
-	 * Add admin pages
-	 *
-	 * @param array $pages
-	 * @return void
-	 * @link http://codex.wordpress.org/Administration_Menus
-	*/
-	public function addAdminPagesReal ()
+
+	public function addAdminPage($type, $page = array())
+	{
+		$this->adminPages(array($type => array($page)));
+	}
+
+	public function adminPages($pages = array())
+	{
+		foreach ($pages as $type => $childs) {
+			for ($i=0; $i<count($childs); $i++) {
+				$page = $this->populatePage($childs[$i]);
+				$this->pages[$type][] = $page;
+			}
+		}
+	}
+
+	public function addAdminPagesReal()
 	{
 		foreach ($this->pages as $type => $pages){
 			for($i=0; $i<count($pages); $i++){
 
 				$page = $pages[$i];
-				$callback =  array($this->Core->Dispatcher, 'adminpage' . '__AYNIL__' . $page['controller'] . '__AYNIL__' . $page['function'] . '__AYNIL__' . $page['menu.slug']);
 
 				switch($type) {
 					case 'menu':
-						add_menu_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $callback, $page['icon'], $page['position'] );
+						add_menu_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $page['callback'], $page['icon'], $page['position'] );
 					break;
 					case 'submenu':
-						add_submenu_page( $page['parent.slug'], $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $callback);
+						add_submenu_page( $page['parent.slug'], $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $page['callback']);
 					break;
 					case 'dashboard':
-						add_dashboard_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $callback);
+						add_dashboard_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $page['callback']);
 					break;
 					case 'posts':
-						add_posts_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $callback);
+						add_posts_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $page['callback']);
 					break;
 					case 'media':
-						add_media_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $callback);
+						add_media_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $page['callback']);
 					break;
 					case 'links':
-						add_links_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $callback);
+						add_links_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $page['callback']);
 					break;
 					case 'pages':
-						add_pages_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $callback);
+						add_pages_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $page['callback']);
 					break;
 					case 'comments':
-						add_comments_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $callback);
+						add_comments_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $page['callback']);
 					break;
 					case 'appearance':
-						add_theme_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $callback);
+						add_theme_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $page['callback']);
 					break;
 					case 'plugins':
-						add_plugins_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $callback);
+						add_plugins_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $page['callback']);
 					break;
 					case 'users':
-						add_users_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $callback);
+						add_users_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $page['callback']);
 					break;
 					case 'tools':
-						add_management_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $callback);
+						add_management_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $page['callback']);
 					break;
 					case 'settings':
-						add_options_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $callback);
+						add_options_page( $page['page.title'], $page['menu.title'], $page['capability'], $page['menu.slug'], $page['callback']);
 					break;
 					default:break;
 				}
@@ -128,120 +94,82 @@ class FramePress_WordPress_001
 
 //-----------------------------------------------------------------------------------------------------
 
-	/**
-	 * Register short codes
-	 *
-	 * @param array $actions
-	 * @return void
-	 * @link http://codex.wordpress.org/Function_Reference/add_shortcode
-	*/
+	public function addShortcode($shortcode)
+	{
+		$this->shortcodes(array($shortcode));
+	}
+
 	public function shortcodes( $shortcodes=array() )
 	{
-		$this->shortcodes = $shortcodes;
-		$short_defaults = array('tag'=> null, 'controller'=> null, 'function'=> null);
-
-		foreach ($this->shortcodes as $sc){
-
-			//populate missing/default info
-			$sc = array_merge($short_defaults, $sc);
-
-			// the name will give dispacher the info to know the responsable
-			// of handle the request: type | controller | function
-			$callback =  array($this->Core->Dispatcher, 'shortcode' . '__AYNIL__' . $sc['controller'] . '__AYNIL__' . $sc['function'] . '__AYNIL__' . $sc['tag']);
-
-			add_shortcode($sc['tag'] , $callback);
+		foreach ($shortcodes as $sc){
+			$this->shortcodes[] = $sc = $this->populateShortcode($sc);
+			add_shortcode($sc['tag'] , $sc['callback']);
 		}
 	}
 
-	/**
-	 * Register plugin hooks
-	 *
-	 * @param array $hooks
-	 * @return void
-	 * @link http://codex.wordpress.org/Function_Reference/add_action
-	 * @link http://codex.wordpress.org/Function_Reference/add_filter
-	*/
+//-----------------------------------------------------------------------------------------------------
+
+	public function addAction($action)
+	{
+		$this->hooks(array($action));
+	}
+
+	public function addFilter($filter)
+	{
+		$this->hooks(array($filter));
+	}
+
 	public function hooks( $hooks=array() )
 	{
-		$this->hooks = $hooks;
-		$hook_defaults =array('is_ajax'=> false, 'priority' => 10, 'accepted_args' => 1);
+		foreach ($hooks as $hook) {
 
-		foreach ($this->hooks as $hook){
-
-			//populate missing/default info
-			$hook = array_merge($hook_defaults, $hook);
-
-			if(!is_array($hook['tag'])){ $hook['tag'] = (array)$hook['tag']; }
-
-			foreach($hook['tag'] as $t){
-
-				// the name will give dispacher the info to know the responsable
-				// of handle the request: type | controller | function
-				$callback =  array($this->Core->Dispatcher, 'hook' . '__AYNIL__' . $hook['controller'] . '__AYNIL__' . $hook['function'] . '__AYNIL__' . $t);
+			$this->hooks[] = $hook = $this->populateHook($hook);
+			foreach ($hook['callback'] as $tag => $callback) {
 
 				if (!$hook['is_ajax']) {
-					add_action($t, $callback, $hook['priority'], $hook['accepted_args']);
+					add_action($tag, $callback, $hook['priority'], $hook['accepted_args']);
 				} else {
 
-					if( in_array($hook['is_ajax'], array(true, 'both', 'private')) ){
-						add_action( 'wp_ajax_' . $t, $callback, $hook['priority'], $hook['accepted_args']);
+					if (in_array($hook['is_ajax'], array(true, 'both', 'private'))) {
+						add_action('wp_ajax_' . $tag, $callback, $hook['priority'], $hook['accepted_args']);
 					}
 
-					if( in_array($hook['is_ajax'], array(true, 'both', 'public')) ){
-						add_action( 'wp_ajax_nopriv_' . $t, $callback, $hook['priority'], $hook['accepted_args']);
+					if (in_array($hook['is_ajax'], array(true, 'both', 'public'))) {
+						add_action('wp_ajax_nopriv_' . $tag, $callback, $hook['priority'], $hook['accepted_args']);
 					}
 				}
 			}
 		}
-		return true;
 	}
 
+//-----------------------------------------------------------------------------------------------------
 
-	/**
-	 * Register metaboxes
-	 *
-	 * @param array $metaboxes
-	 * @return void
-	 * @link http://codex.wordpress.org/Function_Reference/add_meta_box
-	*/
+	public function addMetabox($metabox)
+	{
+		$this->metaboxes(array($metabox));
+	}
+
 	public function metaboxes ($metaboxes = array())
 	{
 		$metab_defaults = array('id'=> null, 'title'=> null, 'post_type'=> null, 'context'=> 'advanced', 'priority'=>null, 'callback_args' => null, 'controller'=> null, 'function'=> null);
 
 		foreach ($metaboxes as $mb){
-			//populate missing/default info
-			$mb = array_merge($metab_defaults, $mb);
+			$mb = $this->populateMetabox($mb);
 			$this->metaboxes[] = $mb;
 		}
-
-		add_action('add_meta_boxes', array($this, 'addMetaboxesReal'));
 	}
 
-	/**
-	 * Add metaboxes
-	 *
-	 * @param string $posttype: posttype begin edited
-	 * @return void
-	 * @link http://codex.wordpress.org/Function_Reference/add_meta_box
-	*/
 	public function addMetaboxesReal ($postType)
 	{
 		foreach ($this->metaboxes as $mb){
-			$callback =  array($this->Core->Dispatcher, 'metabox' . '__AYNIL__' . $mb['controller'] . '__AYNIL__' . $mb['function'] . '__AYNIL__' . $mb['id']);
 			if(in_array($postType, (array)$mb['post_type'])){
-				add_meta_box( $mb['id'], $mb['title'], $callback, $postType, $mb['context'], $mb['priority'], $mb['callback_args'] );
+				add_meta_box( $mb['id'], $mb['title'], $mb['callback'], $postType, $mb['context'], $mb['priority'], $mb['callback_args'] );
 			}
 		}
 	}
 
 	//---------------------------------------
 
-	/**
-	 * Create an URL to a controller or resource using a "place" array
-	 *
-	 * @param array $url place for the href
-	 * @return string
-	*/
 	public function router ($url=array())
 	{
 		//string pased, nothing to do
@@ -293,9 +221,75 @@ class FramePress_WordPress_001
 		return $base . 'page=' . $url['controller'] . $url['function'] . $url['params'];
 	}
 
+	private function populateHook($hook)
+	{
+		//populate missing/default info
+		$hook_defaults = array('tag'=> null, 'controller' => null, 'function' => null, 'is_ajax'=> false, 'priority' => 10, 'accepted_args' => 1);
+		$hook = array_merge($hook_defaults, $hook);
+
+		if (!is_array($hook['tag'])) {
+			$hook['tag'] = (array)$hook['tag'];
+		}
+
+		foreach ($hook['tag'] as $t) {
+			$hook['callback'][$t] = array($this->Core->Dispatcher, 'hook' . '__AYNIL__' . $hook['controller'] . '__AYNIL__' . $hook['function'] . '__AYNIL__' . $t);
+		}
+
+		return $hook;
+	}
+
+	private function populateMetabox($mb)
+	{
+		$metabox_defaults = array('id'=> null, 'title'=> null, 'post_type'=> null, 'context'=> 'advanced', 'priority'=>null, 'callback_args' => null, 'controller'=> null, 'function'=> null);
+		$mb = array_merge($metabox_defaults, $mb);
+		$mb['callback'] = array($this->Core->Dispatcher, 'metabox' . '__AYNIL__' . $mb['controller'] . '__AYNIL__' . $mb['function'] . '__AYNIL__' . $mb['id']);
+		return $mb;
+	}
+
+	private function populateShortcode($sc)
+	{
+		$short_defaults = array('tag'=> null, 'controller'=> null, 'function'=> null);
+		$sc = array_merge($short_defaults, $sc);
+		$sc['callback'] =  array($this->Core->Dispatcher, 'shortcode' . '__AYNIL__' . $sc['controller'] . '__AYNIL__' . $sc['function'] . '__AYNIL__' . $sc['tag']);
+		return $sc;
+	}
+
+	private function populatePage($page= array())
+	{
+		//populate missing/default info
+		$page_defaults = array('page.title'=> null, 'menu.title'=> null, 'capability'=> null, 'controller'=> null, 'function'=>'index', 'parent'=> null, 'icon'=> null, 'position'=> null);
+		$page = array_merge($page_defaults, $page);
+
+		//generate url for image selected
+		if ( $page['icon'] && file_exists( $this->Core->paths['img'] . DS . $page['icon'])) {
+			$page['icon'] =  $this->Core->paths['img.url'] . DS . $page['icon'];
+		}
+
+		//generate slug for this menu
+		$page['menu.slug'] = sanitize_title($this->Core->config['prefix'] . ' ' . $page['menu.title']);
+
+		//find parent menu
+		if ($page['parent']){
+			$menus = (isset($this->pages['menu']))?$this->pages['menu']:array();
+			for ($p=0; $p < count($menus); $p++){
+				if( $menus[$p]['menu.title'] == $page['parent'] ) {
+					$page['parent.slug'] = sanitize_title($this->Core->config['prefix'] . ' ' . $menus[$p]['menu.title']);
+					break;
+				}
+			}
+			if(!isset($page['parent.slug'] )){ $page['parent.slug'] = $page['parent']; }
+		}
+
+		//callback
+		$page['callback'] =  array($this->Core->Dispatcher, 'adminpage' . '__AYNIL__' . $page['controller'] . '__AYNIL__' . $page['function'] . '__AYNIL__' . $page['menu.slug']);
+
+		return $page;
+	}
+
+
 }//end class
 }//end if class exist
 
 //Export framework className
-$GLOBALS["FramePressWordPress"] = 'FramePress_WordPress_001';
-$FramePressWordPress = 'FramePress_WordPress_001';
+$GLOBALS["FramePressWordPress"] = 'FramePress_WordPress_002';
+$FramePressWordPress = 'FramePress_WordPress_002';
